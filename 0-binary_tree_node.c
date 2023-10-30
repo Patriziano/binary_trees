8d0f145939d379374b1c577ed32ec6e9b2e0069a
Patriziano <onahpatrick75@gmail.com>
2023-10-30 02:49:11 -0700

    added betty style doc.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Creates a binary tree mode
+ *
+ * @parent: node parent
+ * @value: node value
+ * Return: pointer to the new node or NULL on failure
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
