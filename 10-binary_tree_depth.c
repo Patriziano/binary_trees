18be36345e936c33adf7098e0440dedaa70e832e
Patriziano <onahpatrick75@gmail.com>
2023-11-01 14:52:53 -0700

    check for when tree is NULL

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,6 +12,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t node_depth;
 
+	if (tree == NULL)
+		return (0);
+
 	node_depth = (tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0;
 	return (node_depth);
 }
